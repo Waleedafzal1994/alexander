462ec8dc277ef335ae8e527c5dc799b3f0c086b0
Asif0011 <asif.hammed@nymcard.com>
2022-06-24 18:05:24 +0500

    CHanges

--- resources/views/services/galleryImages.blade.php
+++ resources/views/services/galleryImages.blade.php
@@ -30,8 +30,19 @@ $userGallery = $service->user->imagesAsArray;
     <!-- Gallery images section -->
     <div id="gallery" class="gallery-section">
         <div class="w-100 bg-white">
-            <img src="https://source.unsplash.com/1600x1200?female,portrait" class="img-responsive">
-            <img src="https://source.unsplash.com/1024x768?female,portrait" class="img-responsive">
+            @foreach ($userGallery as $galleryImage)
+            <img src="{{ $galleryImage->file_name }}" class="img-responsive">
+            <div class="likes gallery-reaction " data-gallery-image-id="{{ $galleryImage->id }}" data-gallery-reaction-id="{{ $galleryImage->likedPost() }}">
+                <span class="gallery-like-heart heart float-left {{ $galleryImage->userliked() ? 'active-heart' : '' }}"><i class="fas fa-heart"></i></span>
+                <span class="liked_gallery_count" id="liked_gallery_count-{{ $galleryImage->id }}">{{ $galleryImage->likes->count() }}</span>
+                @auth
+                @if (Auth::user()->id == $service->user->id)
+                <span class="delete-gallery float-right" id="delete-gallery-{{ $galleryImage->id }}" data-delete-post-id="{{ $galleryImage->id }}" title="Delete"><i class="fas fa-trash"></i></span>
+                @endif
+                @endauth
+            </div>
+            @endforeach
+            <!-- <img src="https://source.unsplash.com/1024x768?female,portrait" class="img-responsive">
             <img src="https://source.unsplash.com/1366x768?female,portrait" class="img-responsive">
             <img src="https://source.unsplash.com/1920x1080?female,portrait" class="img-responsive">
             <img src="https://source.unsplash.com/640x360?female,portrait" class="img-responsive">
@@ -44,7 +55,7 @@ $userGallery = $service->user->imagesAsArray;
             <img src="https://source.unsplash.com/1100x1600?female,portrait" class="img-responsive">
             <img src="https://source.unsplash.com/1600x1100?female,portrait" class="img-responsive">
             <img src="https://source.unsplash.com/992x768?female,portrait" class="img-responsive">
-            <img src="https://source.unsplash.com/768x992?female,portrait" class="img-responsive">
+            <img src="https://source.unsplash.com/768x992?female,portrait" class="img-responsive"> -->
         </div>
     </div>
 
