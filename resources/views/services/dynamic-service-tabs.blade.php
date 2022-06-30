9d8f612b1c5eb69dee52a802cc4ca8c872a82672
Salman Tariq <75684584+Salman6432@users.noreply.github.com>
2022-06-30 17:06:35 +0500

    carousel - arrows - heading - gift img fixed

--- resources/views/services/dynamic-service-tabs.blade.php
+++ resources/views/services/dynamic-service-tabs.blade.php
@@ -23,9 +23,9 @@
                     </span>
                 </div>
                 <div class="served ml-3">
-                    <button class="new-btn bg-purple-gradient text-white border-0 py-3 br-10 h-40 number-row-card">
+                    <div class="new-btn bg-purple-gradient text-white border-0 py-3 br-10 h-40 number-row-card">
                         <i class="fas fa-star"></i> 5.0 <span class="dot"> . </span> 1258 Served
-                    </button>
+                    </div>
                 </div>
             </div>
         </div>
