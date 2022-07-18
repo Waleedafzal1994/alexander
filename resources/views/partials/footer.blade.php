cd3e1defd6339683fe9d8f9e9becfe2410c25aeb
Salman Tariq <75684584+Salman6432@users.noreply.github.com>
2022-07-18 19:43:50 +0500

    welcome Carousel fixed - footer fixed

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,19 +2,21 @@
     <div id="footer">
         <div class="container" style="padding:15px;">
             <div class="row">
-                <div class="col-md-5">
+                <div class="col-md-6">
                     <h6 class="special-heading">About <span>Us</span></h6>
-                    <p style="text-align: left; margin:10px 0;">GamersPlay is a premium website where you can find a
+                    <p style="text-align: left; margin-top:10px;">GamersPlay is a premium website where you can find a
                         gamer friend. Chat, play and interact with our members or become a GamersPlay+ and offer your own
                         gaming and lifestyle services !</p>
+                </div>
+                <div class="col-md-6 d-flex align-items-center justify-content-md-center">
                     <div style="display:flex;" class="mobile_ul_nav">
-                        <ul class="footer-menu">
+                        <ul class="footer-menu my-0">
                             <li><a href="/">Home</a></li>
                             {{-- <li><a href="/services">Services</a></li> --}}
                             <li><a href="#">Services</a></li>
                             <li><a href="/news">News</a></li>
                         </ul>
-                        <ul class="footer-menu">
+                        <ul class="footer-menu my-0">
                             <li><a href="/terms-of-service">Terms of Service</a></li>
                             <li><a href="/privacy-policy">Privacy Policy</a></li>
                             <li><a href="/community-guidelines">Community Guidelines</a></li>
@@ -48,38 +50,33 @@
     <div style="background:black; color:White;">
         <div class="container footer-mobile" style="padding:15px;">
             <div class="row">
-                <div class="col-md-6">
-                    <div class="row" style="align-items:center;">
-                        <div class="col-md-4">
-                            <h6 class="special-heading" style="display:inline-block; margin:0; padding:0;">
-                                Gamers<span>Play</span></h6>
-                        </div>
-                        <div class="col-md-8">
-                            <small>Copyright © {{ date('Y') }} GamersPlay - All rights reserved.</small>
-                        </div>
+                <div class="col-md-7">
+                    <div class="d-flex align-items-center">
+                        <h6 class="special-heading mr-5 mb-0">
+                            Gamers<span>Play</span>
+                        </h6>
+                        <small>Copyright © {{ date('Y') }} GamersPlay - All rights reserved.</small>
                     </div>
 
                 </div>
-                <div class="col-md-6">
-                    <div class="row">
-                        <div class="col-md-2 col-sm-12">
-                            <a href="#" class="footer-link active">Home</a>
-                        </div>
-                        <div class="col-md-2">
-                            {{-- <a href="/services" class="footer-link">Services</a> --}}
-                            <a href="#" class="footer-link">Services</a>
-                        </div>
-                        <div class="col-md-2">
-                            <a href="/news" class="footer-link">News</a>
-                        </div>
-                        <div class="col-md-2">
-                            <div style="display:flex;" class="mobile-center">
-                                <a href="https://www.facebook.com/gamersplaygg"><img src="/imgs/icons/facebook.svg"
-                                        class="footer-socials-icon"></a>
-                                <a href="#"><img src="/imgs/icons/instagram.svg" class="footer-socials-icon"></a>
-                                <a href="https://twitter.com/gamersplaygg"><img src="/imgs/icons/twitter.svg"
-                                        class="footer-socials-icon"></a>
-                            </div>
+                <div class="footer-social col-md-5 d-flex align-items-center">
+                    <div class="mr-3">
+                        <a href="#" class="footer-link active">Home</a>
+                    </div>
+                    <div class="mr-3">
+                        {{-- <a href="/services" class="footer-link">Services</a> --}}
+                        <a href="#" class="footer-link">Services</a>
+                    </div>
+                    <div class="mr-3">
+                        <a href="/news" class="footer-link">News</a>
+                    </div>
+                    <div class="mr-3">
+                        <div style="display:flex;" class="mobile-center">
+                            <a href="https://www.facebook.com/gamersplaygg"><img src="/imgs/icons/facebook.svg"
+                                    class="footer-socials-icon"></a>
+                            <a href="#"><img src="/imgs/icons/instagram.svg" class="footer-socials-icon"></a>
+                            <a href="https://twitter.com/gamersplaygg"><img src="/imgs/icons/twitter.svg"
+                                    class="footer-socials-icon"></a>
                         </div>
                     </div>
                 </div>
